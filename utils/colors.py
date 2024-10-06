5afd720123eb4c28dd5f71a9dc08f5d572f90654
Anderson <62722855+anderdot@users.noreply.github.com>
2024-10-06 20:47:29 -0300

    feat: add string_color and end parameter

--- utils/colors.py
+++ utils/colors.py
@@ -1,23 +1,23 @@
 from enum import Enum
 
 class Color(Enum):
-    reset       = '\033[m'
-    white       = '\033[30m'
-    red         = '\033[31m'
-    green       = '\033[32m'
-    yellow      = '\033[33m'
-    blue        = '\033[34m'
-    purple      = '\033[35m'
-    cyan        = '\033[36m'
-    gray        = '\033[37m'
+    reset     = '\033[m'
+    white     = '\033[30m'
+    red       = '\033[31m'
+    green     = '\033[32m'
+    yellow    = '\033[33m'
+    blue      = '\033[34m'
+    purple    = '\033[35m'
+    cyan      = '\033[36m'
+    gray      = '\033[37m'
 
-    bold        = '\033[1m'
-    underline   = '\033[4m'
+    bold      = '\033[1m'
+    underline = '\033[4m'
 
     def __str__(self):
         return self.value
 
-def print_color(text, color=Color.white, styles=None):
+def print_color(text, color=Color.white, styles=None, end='\n'):
     """Print text with specific color and styles.
 
     Args:
@@ -29,4 +29,18 @@ def print_color(text, color=Color.white, styles=None):
         styles = []
 
     styled_text = f"{''.join([str(style) for style in styles])}{text}"
-    print(f"{color}{styled_text}{color.reset}")
+    print(f"{color}{styled_text}{color.reset}", end=end)
+
+def string_color(text, color=Color.white, styles=None):
+    """Return a string with specific color and styles.
+
+    Args:
+        text (str): Text to print.
+        color (Color, optional): Color to use. Defaults to Color.white.
+        styles (list[Color], optional): List of styles to apply. Defaults to None.
+    """
+    if styles is None:
+        styles = []
+
+    styled_text = f"{''.join([str(style) for style in styles])}{text}"
+    return f"{color}{styled_text}{color.reset}"
