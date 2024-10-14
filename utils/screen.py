e7c4f9b7bdeaf0c01e4589f7095f7978aa5c3f4e
Anderson <62722855+anderdot@users.noreply.github.com>
2024-10-14 17:25:28 -0300

    feat: cmd window size

--- utils/screen.py
+++ utils/screen.py
@@ -3,3 +3,12 @@ import os
 def clear_screen():
     """Clears the terminal screen."""
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def set_cmd_window_size(cols, lines):
+    """Sets the size of the command prompt (CMD) window in Windows.
+
+    Args:
+        cols (int): The number of columns (width) for the CMD window.
+        lines (int): The number of lines (height) for the CMD window.
+    """
+    os.system(f'mode con: cols={cols} lines={lines}')
